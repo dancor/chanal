bf05262167c211ff541bff4cab7bb640e04bb403
Daniel Corson <danl@alum.mit.edu>
2009-12-13 20:39:38 -0800

    starting prog

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,23 @@
 module Main where
 
+import Control.Applicative
+import FUtil
+import System.IO
+import qualified Data.Map as M
+
+type L = M.Map String [L]
+
+addLines :: L -> [(String, String)] -> L
+addLines m [] = m
+addLines m [l:rest] =
+  if tail l == 'T'
+    then
+    else
+
 main :: IO ()
 main = do
-  putStrLn "hi"
+  t <- addLines M.empty .
+    map (\ [a, b] -> (a, b)) . splitN 2 . filter (not . null) . lines <$>
+    readFile "games/lines"
+  print t
+
